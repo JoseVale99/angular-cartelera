9ec61ebf2bbeee80ec27bea942e56a5d0bf61b32
JoseVale99 <valezarate.9903@gmail.com>
2025-01-13 16:10:59 -0600

    Enhance Navbar component with theme color functionality and scroll detection. Added color theme selection feature with event emitter and implemented scroll event listener to toggle navbar style based on scroll position.

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
+import { MatAnchor, MatIconButton } from '@angular/material/button';
+import { themeColors } from '../../constants/theme-colors';
+import { Color } from '../../enums/colors.enum';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatMenuModule,
+    RouterLinkActive,
+    NgOptimizedImage,
+    NgForOf,
+    MatIconModule,
+    MatAnchor,
+    MatIconButton,
+    RouterLink
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @Output() changeColorTheme: EventEmitter<string> = new EventEmitter();
+
+  themeColorList = themeColors;
+  themeColorInit: string = Color.RED;
+
+  isScrolled = false;
+
+  @HostListener('window:scroll')
+  scrollEvent() {
+    this.isScrolled = window.scrollY >= 30;
+  }
+  
+  setColorTheme(color: string) {
+    this.themeColorInit = color;
+    this.changeColorTheme.emit(color);
+  }
 
 }
