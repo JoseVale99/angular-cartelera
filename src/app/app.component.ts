489cc15be02b4a99bb6b4803c210f321d9410fa3
JoseVale99 <valezarate.9903@gmail.com>
2025-01-14 01:29:42 -0600

    feat: enhance app component with theme color functionality and layout structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { NgClass } from '@angular/common';
+import { themeColors } from './core/constants/theme-colors';
+import { Color } from './core/enums/colors.enum';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-cartelera-movies';
+  themeColorList = themeColors;
+  themeColorEnum = Color;
+  themeColorInit: string = Color.RED;
+
+  constructor( private router: Router) {}
+
+  changeColorTheme(color: string): void {
+    this.themeColorInit = color;
+  }
+  checkSelectedTheme(color: string) {
+    return this.themeColorInit === color;
+  }
+
 }
