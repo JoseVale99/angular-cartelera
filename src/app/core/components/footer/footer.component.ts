a1a022619c264f721432df74be1862bbbf21c3ff
JoseVale99 <valezarate.9903@gmail.com>
2025-01-14 22:12:38 -0600

    feat(footer): redesign footer component with social media links and Tailwind CSS styling

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,19 @@
+import { NgForOf, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  socialData = [
+    { name: 'GitHub', url: 'https://github.com/JoseVale99', img: 'assets/svg/github.svg' },
+    { name: 'Twitter', url: 'https://twitter.com/ZarateCarreno', img: 'assets/svg/twitter.svg' },
+  ];
 }
