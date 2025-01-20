2299ba38ea110522afc8b86180fd5e16e386c218
JoseVale99 <valezarate.9903@gmail.com>
2025-01-20 12:54:42 -0600

    refactor(movies): update MoviesService to use lastValueFrom for better promise handling

--- src/app/features/content/services/movies.service.ts
+++ src/app/features/content/services/movies.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-  baseUrl = environment.BASE_URL + 'api/rest/';
+  private readonly baseUrl = environment.BASE_URL + 'api/rest/';
   private http = inject(HttpClient);
 
   /*
@@ -14,6 +15,6 @@ export class MoviesService {
   */
   getSliderMovies(type: string, listen: number) {
     const $response = this.http.get(this.baseUrl + `slider?type=${type}&listen=${listen}`);
-    return $response;
+    return lastValueFrom($response);
   }
 }
