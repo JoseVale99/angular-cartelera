fde6d444e69f747260b7eaecc6fa217252ca5c4a
JoseVale99 <valezarate.9903@gmail.com>
2025-01-20 17:14:17 -0600

    fix(home): add console log for getSliderMovies method in HomeComponent to aid debugging

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -48,10 +48,10 @@ export class HomeComponent{
   }
 
   private async getSliderMovies() {
+    console.log('getSliderMovies');
     try {
       const { data } = await this.moviesService.getSliderMovies('movies', 0) as { data: MovieSliderModel[] };
       this.moviesList = data;
-      console.log(this.moviesList);
     } catch (error) {
       console.error(error);
     }
