db7beabfbe331489d5e3620c5998045761538ee9
JoseVale99 <valezarate.9903@gmail.com>
2025-01-23 13:58:26 -0600

    feat(app.config): add locale support for Spanish (Mexico) and update providers for localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
+import { registerLocaleData } from '@angular/common';
+import localeEsMx from '@angular/common/locales/es-MX';
+
+registerLocaleData(localeEsMx);
 
 register();
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop'),
-    provideHttpClient()
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+    { provide: LOCALE_ID, useValue: 'es-MX' }, provideAnimationsAsync('noop'), provideHttpClient()
   ]
 };
