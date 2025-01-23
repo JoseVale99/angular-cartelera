363f96b485fb00f75c276c5d7a94e13a3701254d
JoseVale99 <valezarate.9903@gmail.com>
2025-01-23 15:41:29 -0600

    refactor(poster-card): remove unused imports and add circular progress component for enhanced functionality

--- src/app/shared/components/poster-card/poster-card.component.ts
+++ src/app/shared/components/poster-card/poster-card.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ImgMissingDirective } from '../../directives/img-missing.directive';
-import { DatePipe, NgIf } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { environment } from '../../../../environments/environment';
+import { CircularProgressComponent } from '../circular-progress/circular-progress.component';
 
 @Component({
   selector: 'app-poster-card',
   imports: [
+    CircularProgressComponent,
     RouterLink,
-    ImgMissingDirective,
-    DatePipe,
-    NgIf
+    DatePipe
   ],
   templateUrl: './poster-card.component.html',
   styleUrl: './poster-card.component.css'
