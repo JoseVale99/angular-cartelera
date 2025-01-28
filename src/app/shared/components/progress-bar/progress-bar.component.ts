16eefba8ca312279e24a7c389349c825ccc5be10
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 18:58:12 -0600

    feat(progress-bar): add ProgressBarComponent with basic template

---
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  imports: [],
+  template: `
+    <p>
+      progress-bar works!
+    </p>
+  `,
+  styles: ``
+})
+export class ProgressBarComponent {
+
+}
