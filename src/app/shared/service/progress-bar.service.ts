60aab5c7f8ceea5f06ba0c748fac56c62edd9d58
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 19:00:10 -0600

    feat(progress-bar): add service for managing progress bar visibility

--- src/app/shared/service/progress-bar.service.ts
+++ src/app/shared/service/progress-bar.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressBarService {
+  public isLoading = signal(false);
 
-  constructor() { }
+  public show() {
+    this.isLoading.set(true);
+  }
+
+  public hide() {
+    this.isLoading.set(false);
+  }
 }
