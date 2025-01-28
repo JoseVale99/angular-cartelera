1cbbcdc66caf1874434b151f97d36366d705a7b5
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 18:58:48 -0600

    feat(progress-bar): integrate ProgressBarService and update template for loading state

--- src/app/shared/components/progress-bar/progress-bar.component.ts
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ProgressBarService } from '../../service/progress-bar.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-progress-bar',
-  imports: [],
+  imports: [
+    MatProgressBarModule
+  ],
   template: `
-    <p>
-      progress-bar works!
-    </p>
+    @if (isLoading()) {
+      <mat-progress-bar mode="indeterminate"></mat-progress-bar>
+    }
   `,
   styles: ``
 })
 export class ProgressBarComponent {
-
+  private progressBarService = inject(ProgressBarService);
+  public isLoading = computed(() => this.progressBarService.isLoading());
 }
