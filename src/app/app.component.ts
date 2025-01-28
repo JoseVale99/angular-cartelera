30cd18a7f0245d4029ce57b9e54723e878894946
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 20:41:29 -0600

    refactor(app): simplify constructor in AppComponent for clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { NgClass } from '@angular/common';
 import { themeColors } from './core/constants/theme-colors';
 import { Color } from './core/enums/colors.enum';
-import { ProgressBarService } from './shared/service/progress-bar.service';
-import { ProgressBarComponent } from './shared/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass, ProgressBarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,8 +16,6 @@ export class AppComponent {
   themeColorList = themeColors;
   themeColorEnum = Color;
   themeColorInit: string = Color.RED;
-  private progressBarService = inject(ProgressBarService);
-  public isLoading = computed(() => this.progressBarService.isLoading());
 
   constructor( private router: Router) {}
 
