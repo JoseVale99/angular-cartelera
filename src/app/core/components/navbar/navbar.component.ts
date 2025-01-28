9f84b3cc8c9f1ca902790d60c15867eca66c9eb8
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 20:43:04 -0600

    refactor(navbar): improve logic and state management in NavbarComponent

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { Component, computed, EventEmitter, HostListener, inject, Output } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule, NgForOf } from '@angular/common';
@@ -6,7 +6,8 @@ import { MatAnchor, MatIconButton } from '@angular/material/button';
 import { themeColors } from '../../constants/theme-colors';
 import { Color } from '../../enums/colors.enum';
 import { MatIconModule } from '@angular/material/icon';
-
+import { ProgressBarService } from '../../../shared/service/progress-bar.service';
+import { ProgressBarComponent } from '../../../shared/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-navbar',
@@ -18,13 +19,16 @@ import { MatIconModule } from '@angular/material/icon';
     MatIconModule,
     MatAnchor,
     MatIconButton,
-    RouterLink
+    RouterLink,
+    ProgressBarComponent
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   @Output() changeColorTheme: EventEmitter<string> = new EventEmitter();
+  private progressBarService = inject(ProgressBarService);
+  public isLoading = computed(() => this.progressBarService.isLoading());
 
   isMenuOpen = false;
 
