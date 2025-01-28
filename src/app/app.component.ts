070829eb315cfc5d70168345b8a651f11b70131e
JoseVale99 <valezarate.9903@gmail.com>
2025-01-27 20:09:14 -0600

    feat(app): add loading state management using ProgressBarService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { NgClass } from '@angular/common';
 import { themeColors } from './core/constants/theme-colors';
 import { Color } from './core/enums/colors.enum';
+import { ProgressBarService } from './shared/service/progress-bar.service';
+import { ProgressBarComponent } from './shared/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass, ProgressBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +18,8 @@ export class AppComponent {
   themeColorList = themeColors;
   themeColorEnum = Color;
   themeColorInit: string = Color.RED;
+  private progressBarService = inject(ProgressBarService);
+  public isLoading = computed(() => this.progressBarService.isLoading());
 
   constructor( private router: Router) {}
 
