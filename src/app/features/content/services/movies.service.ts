cb2f51cb4ea7fd20269cb9ada1eb7a27b233fb0c
JoseVale99 <valezarate.9903@gmail.com>
2025-04-01 21:14:04 -0600

    feat(movies.service): add getAllMovies method for paginated movie retrieval

--- src/app/features/content/services/movies.service.ts
+++ src/app/features/content/services/movies.service.ts
@@ -17,4 +17,16 @@ export class MoviesService {
     const $response = this.http.get(this.baseUrl + `sliders?type=${type}&listing=${listen}`);
     return lastValueFrom($response);
   }
+
+  /**
+   *  Get paginated movies
+   * @param page
+   * @param genres
+   * @param years
+   * @param order
+  */
+  getAllMovies(page: number, genres: string, years: string, order: string) {
+    const $response = this.http.get(this.baseUrl + `listing?post_type=movies&page=${page}&genres=${genres}&years=${years}&order=${order}`);
+    return lastValueFrom($response);
+  }
 }
