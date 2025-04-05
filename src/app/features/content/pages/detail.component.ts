760fbffb52e75db2fe37cf86e921471c937d674e
JoseVale99 <valezarate.9903@gmail.com>
2025-04-05 15:44:22 -0600

    Refactor DetailComponent to use Angular's dependency injection for ActivatedRoute and Router, and implement logic to handle route parameters for content type identification.

--- src/app/features/content/pages/detail.component.ts
+++ src/app/features/content/pages/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './detail.component.css'
 })
 export class DetailComponent {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private contentType! : string;
 
+
+  constructor() {
+    this.contentType = this.router.url.split('/')[1];
+  }
+
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      params => {
+        const id = params['id'];
+        console.log(id);
+        if (this.contentType === 'movies') {
+        } else if (this.contentType === 'tv-shows') {
+        }
+      }
+    );
+  }
 }
