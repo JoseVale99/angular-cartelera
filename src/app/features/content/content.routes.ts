d1c77341c0f4292482be3306a3f6144d548e1051
JoseVale99 <valezarate.9903@gmail.com>
2025-04-05 15:35:46 -0600

    Add DetailComponent with HTML template and CSS styles, and update routing to include detail view

--- src/app/features/content/content.routes.ts
+++ src/app/features/content/content.routes.ts
@@ -1,11 +1,12 @@
 import {Route} from '@angular/router';
 import { ContentMainComponent } from "./content.component";
+import { DetailComponent } from './pages/detail.component';
 
 export const CONTENT_ROUTES: Route[] = [
   {path: '',
     children: [
       { path: '', component: ContentMainComponent},
-     // { path: ':url', component: DetailComponent}
+     { path: ':url', component: DetailComponent}
     ]},
 ];
 
