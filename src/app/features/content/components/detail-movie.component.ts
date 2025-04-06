54bbf938e45078e8c0c9aa0a539d6d6c9c739960
JoseVale99 <valezarate.9903@gmail.com>
2025-04-06 02:00:24 -0600

    Enhance DetailMovieComponent by adding logo and gallery image handling, integrating Swiper for gallery display, and improving image loading logic. Update component schema to support custom elements.

--- src/app/features/content/components/detail-movie.component.ts
+++ src/app/features/content/components/detail-movie.component.ts
@@ -1,16 +1,20 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, input, signal } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
 import { DetailMedia } from '../interfaces/detail.interface';
 import { environment } from '../../../../environments/environment';
 import { DatePipe } from '@angular/common';
+import { SwiperDirective } from '../../../shared/directives/swiper.directive';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-detail-movie',
   imports: [
     DatePipe
+    
   ],
   templateUrl: './detail-movie.component.html',
-  styleUrl: './detail-movie.component.css'
+  styleUrl: './detail-movie.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DetailMovieComponent {
     private moviesService = inject(MoviesService);
@@ -19,19 +23,33 @@ export class DetailMovieComponent {
     public isLoading = signal(false);
     public dataDetail! : DetailMedia;
     public imageUrl!: string;
-    public showFullDescription = false;
+    public logoUrl!: string;
     private readonly baseUrl = environment.urlBase + 'wp-content/uploads';
+    public urlGallery = environment.urlGallery;
+    public showFullDescription = false;
+    public imageGallery: string[] = [];
 
-  ngOnChanges() {
+    ngOnInit() {
     this.getMovieBySlugDetail(this.slug()!);
   }
+
+  swiperConfig:  SwiperOptions = {
+    initialSlide: 0,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    }
+  };
 
   private async getMovieBySlugDetail(slug: string) {
     this.isLoading.set(true);
     try {
       const data = await this.moviesService.getMovieBySlug(slug) as { data: DetailMedia };
       this.dataDetail = data.data;
-      await this.loadImage();
+      this.logoUrl = this.loadImage(this.dataDetail.images?.logo);
+      this.imageUrl = this.loadImage(this.dataDetail.images?.poster);
+      this.imageGallery = this.dataDetail.gallery.split('\n').map(url => url.trim()).filter(trimmedUrl => trimmedUrl).map(trimmedUrl => this.urlGallery + trimmedUrl);
+      console.log(this.imageGallery);
       this.isLoading.set(false);
     } catch (error) {
       console.error(error);
@@ -39,13 +57,16 @@ export class DetailMovieComponent {
     }
   }
 
-  async loadImage() {
-    const uuid = this.dataDetail.images?.poster;
-    this.imageUrl = uuid ? `${this.baseUrl}${uuid}` : '/assets/img/fallback.webp';
+  private loadImage(uuid: string) {
+    return `${this.baseUrl}${uuid}`;
   }
 
   onImageError() {
     this.imageUrl = '/assets/img/fallback.webp';
+  }
+
+  onImageErrorLogo() {
+    this.logoUrl = '';
   }
 
   getGenreName(id: number): string {
