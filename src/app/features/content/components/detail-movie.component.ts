99c1ceba6ce866113d2590a7777e1087feec041f
JoseVale99 <valezarate.9903@gmail.com>
2025-04-06 20:11:41 -0600

    Enhance DetailMovieComponent by adding a video player with options for different video sources, implementing a toggle for options display, and improving URL sanitization for video playback. Update template structure for better layout and user interaction.

--- src/app/features/content/components/detail-movie.component.ts
+++ src/app/features/content/components/detail-movie.component.ts
@@ -4,35 +4,56 @@ import { DetailMedia } from '../interfaces/detail.interface';
 import { environment } from '../../../../environments/environment';
 import { DatePipe } from '@angular/common';
 import { SwiperOptions } from 'swiper/types';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { VideoSource } from '../interfaces/player-video.interface';
 
 @Component({
   selector: 'app-detail-movie',
   imports: [
     DatePipe
-    
   ],
   templateUrl: './detail-movie.component.html',
   styleUrl: './detail-movie.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('300ms ease-in-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in-out', style({ transform: 'translateX(-100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class DetailMovieComponent {
-    private moviesService = inject(MoviesService);
-    public slug = input<string>();
-    public genres = input<any[]>();
-    public isLoading = signal(false);
-    public dataDetail! : DetailMedia;
-    public imageUrl!: string;
-    public logoUrl!: string;
-    private readonly baseUrl = environment.urlBase + 'wp-content/uploads';
-    public urlGallery = environment.urlGallery;
-    public showFullDescription = false;
-    public imageGallery: string[] = [];
+  private moviesService = inject(MoviesService);
+  private domSanitizer = inject(DomSanitizer);
+  public slug = input<string>();
+  public genres = input<any[]>();
+  public isLoading = signal(false);
+  public dataDetail!: DetailMedia;
+  public imageUrl!: string;
+  public logoUrl!: string;
+  private readonly baseUrl = environment.urlBase + 'wp-content/uploads';
+  public urlGallery = environment.urlGallery;
+  public showFullDescription = false;
+  public imageGallery: string[] = [];
 
-    ngOnInit() {
+  showOptions = false;
+
+  videoUrl!: SafeResourceUrl;
+
+  opciones: VideoSource[] = [];
+
+
+  ngOnInit() {
     this.getMovieBySlugDetail(this.slug()!);
   }
 
-  swiperConfig:  SwiperOptions = {
+  swiperConfig: SwiperOptions = {
     initialSlide: 0,
     autoplay: {
       delay: 5000,
@@ -48,7 +69,20 @@ export class DetailMovieComponent {
       this.logoUrl = this.loadImage(this.dataDetail.images?.logo);
       this.imageUrl = this.loadImage(this.dataDetail.images?.poster);
       this.imageGallery = this.dataDetail.gallery.split('\n').map(url => url.trim()).filter(trimmedUrl => trimmedUrl).map(trimmedUrl => this.urlGallery + trimmedUrl);
-      console.log(this.imageGallery);
+      this.isLoading.set(false);
+      this.getUrlsPlayerVideo(this.dataDetail._id);
+    } catch (error) {
+      console.error(error);
+      this.isLoading.set(false);
+    }
+  }
+
+  private async getUrlsPlayerVideo(id: number) {
+    this.isLoading.set(true);
+    try {
+      const data = await this.moviesService.getUrlsPlayer(id) as { data: VideoSource[] };
+      this.opciones = data.data;
+      this.videoUrl = this.sanitizeUrl(this.opciones[0].url);
       this.isLoading.set(false);
     } catch (error) {
       console.error(error);
@@ -77,5 +111,20 @@ export class DetailMovieComponent {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = Math.round(totalMinutes % 60);
     return `${hours}h ${minutes}m`;
+  }
+
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  toggleOptions() {
+    this.showOptions = !this.showOptions;
+  }
+
+  changeVideoPlayer(url: string) {
+    this.isLoading.set(true);
+    this.videoUrl = this.sanitizeUrl(url);
+    this.showOptions = false;
+    this.isLoading.set(false);
   }
 }
