ea88c2e508ab17d2019149bd204622c4819e051a
JoseVale99 <valezarate.9903@gmail.com>
2025-05-25 03:33:41 -0600

    Refactor PosterCardComponent to utilize Angular signals and computed properties for dynamic image loading and link generation. Update HTML template for improved readability and maintainability.

--- src/app/shared/components/poster-card/poster-card.component.ts
+++ src/app/shared/components/poster-card/poster-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnChanges } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { environment } from '../../../../environments/environment';
@@ -14,29 +14,26 @@ import { CircularProgressComponent } from '../circular-progress/circular-progres
   templateUrl: './poster-card.component.html',
   styleUrl: './poster-card.component.css'
 })
-export class PosterCardComponent implements OnChanges {
+export class PosterCardComponent {
   model = input<any>();
   isMovie = input<boolean>();
   private readonly baseUrl = environment.urlBase + 'wp-content/uploads';
-  linkPath: string = '/';
-  imageUrl!: string;
-
-  ngOnChanges() {
-    this.loadImage();
-    this.getLinkPath();
-  }
-
-  getLinkPath() {
+  private imageError = signal(false);
+  linkPath = computed(() => {
     const slug = this.model()?.slug;
-    this.linkPath = slug ? (this.isMovie() ? `/movies/${slug}` : `/tvshows/${slug}`) : '/';
-  }
+    return slug  ? this.isMovie() ? `/movies/${slug}` : `/tvshows/${slug}` : '/';
+  });
 
-  loadImage() {
+  imageUrl = computed(() => {
     const uuid = this.model()?.images?.poster;
-    this.imageUrl = uuid ? `${this.baseUrl}${uuid}` : '/assets/img/fallback.webp';
-  }
+    return uuid ? `${this.baseUrl}${uuid}` : '/assets/img/fallback.webp';
+  });
+
+  displayedImageUrl = computed(() => { 
+    return this.imageError() ? '/assets/img/fallback.webp' : this.imageUrl(); 
+  });
 
   onImageError() {
-    this.imageUrl = '/assets/img/fallback.webp';
+    this.imageError.set(true);
   }
 }
