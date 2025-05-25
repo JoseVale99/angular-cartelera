9f489cf382e20fdf44d72062bf458433e1061a66
JoseVale99 <valezarate.9903@gmail.com>
2025-05-25 02:48:33 -0600

    Add VideoPlayerComponent with HTML template and TypeScript logic for video playback functionality. Implement options for selecting video sources and loading indicators for improved user experience.

---
+++ src/app/features/content/components/video-player/video-player.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject, input, signal } from '@angular/core';
+import { VideoSource } from '../../interfaces/player-video.interface';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { MediaService } from '../../services/media.service';
+
+@Component({
+  selector: 'app-video-player',
+  imports: [],
+  templateUrl: './video-player.component.html',
+  styles: ``
+})
+export class VideoPlayerComponent {
+  public idPost = input.required<number>();
+  public title = input.required<string>();
+  private mediaService = inject(MediaService);
+  private domSanitizer = inject(DomSanitizer);
+  public selectedOption!: string;
+  public isLoading = signal(false);
+  private videoCache: Map<string, SafeResourceUrl> = new Map();
+  showOptions = false;
+  videoUrl!: SafeResourceUrl;
+  opciones: VideoSource[] = [];
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.getUrlsPlayerVideo(this.idPost())
+    }, 0);
+  }
+  private async getUrlsPlayerVideo(id: number) {
+    this.isLoading.set(true);
+    try {
+      const data = await this.mediaService.getUrlsPlayer(id) as { data: VideoSource[] };
+      this.opciones = data.data;
+      this.videoUrl = this.sanitizeUrl(this.opciones[0].url);
+      this.selectedOption = this.opciones[0].url;
+      this.isLoading.set(false);
+    } catch (error) {
+      console.error(error);
+      this.isLoading.set(false);
+    }
+  }
+
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  toggleOptions() {
+    this.showOptions = !this.showOptions;
+  }
+
+  changeVideoPlayer(url: string) {
+    this.isLoading.set(true);
+    if (this.videoCache.has(url)) {
+      this.videoUrl = this.videoCache.get(url)!;
+    } else {
+      this.videoUrl = this.sanitizeUrl(url);
+      this.videoCache.set(url, this.videoUrl);
+    }
+    this.selectedOption = url;
+    this.showOptions = false;
+    this.isLoading.set(false);
+  }
+}
