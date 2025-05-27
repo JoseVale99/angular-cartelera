83b26b0f056b7cc522716a266a0675e3a74dff80
JoseVale99 <valezarate.9903@gmail.com>
2025-05-27 00:24:52 -0600

    Refactor PosterCardComponent to enhance image handling by integrating ImageFallbackComponent for improved error management. Update HTML structure to adjust image dimensions and maintain responsiveness, ensuring a better user experience.

--- src/app/shared/components/poster-card/poster-card.component.ts
+++ src/app/shared/components/poster-card/poster-card.component.ts
@@ -1,39 +1,25 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { environment } from '../../../../environments/environment';
 import { CircularProgressComponent } from '../circular-progress/circular-progress.component';
+import { ImageFallbackComponent } from "../image-fallback/image-fallback.component";
 
 @Component({
   selector: 'app-poster-card',
   imports: [
     CircularProgressComponent,
     RouterLink,
-    DatePipe
-  ],
+    DatePipe,
+    ImageFallbackComponent
+],
   templateUrl: './poster-card.component.html',
   styleUrl: './poster-card.component.css'
 })
 export class PosterCardComponent {
   model = input<any>();
   isMovie = input<boolean>();
-  private readonly baseUrl = environment.urlBase + 'wp-content/uploads';
-  private imageError = signal(false);
   linkPath = computed(() => {
     const slug = this.model()?.slug;
     return slug  ? this.isMovie() ? `/movies/${slug}` : `/tvshows/${slug}` : '/';
   });
-
-  imageUrl = computed(() => {
-    const uuid = this.model()?.images?.poster;
-    return uuid ? `${this.baseUrl}${uuid}` : '/assets/img/fallback.webp';
-  });
-
-  displayedImageUrl = computed(() => { 
-    return this.imageError() ? '/assets/img/fallback.webp' : this.imageUrl(); 
-  });
-
-  onImageError() {
-    this.imageError.set(true);
-  }
 }
